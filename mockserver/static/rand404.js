ba746a6490c91eaa8097a5ce6a1bfc3be7beb94c
Aaron Menadue <aaron.menadue@gmail.com>
2025-10-31 03:57:58 +1030

    made edits to example pages

---
+++ mockserver/static/rand404.js
@@ -0,0 +1,68 @@
+var preloaded = document.onload ?? window.onload;
+
+document.onload = window.onload = () => {
+	if (preloaded)
+		preloaded();
+	var words = [
+		"apple",
+		"river",
+		"mountain",
+		"city",
+		"car",
+		"computer",
+		"book",
+		"garden",
+		"window",
+		"chair",
+		"teacher",
+		"student",
+		"bridge",
+		"camera",
+		"coffee",
+		"planet",
+		"forest",
+		"animal",
+		"music",
+		"cloud",
+		"train",
+		"door",
+		"flower",
+		"island",
+		"machine",
+		"painting",
+		"desk",
+		"road",
+		"ocean",
+		"clock",
+		"bottle",
+		"library",
+		"engine",
+		"village",
+		"market",
+		"tree",
+		"storm",
+		"language",
+		"phone",
+		"station",
+		"riverbank",
+		"journal",
+		"mirror",
+		"signal",
+		"ship",
+		"house",
+		"street",
+		"valley",
+		"museum",
+		"shadow",
+		"dream",
+	];
+
+	var randomEndpoint = "";
+	for (var i = 0; i < 3; i++) {
+		var selection = Math.floor(Math.random() * words.length);
+		var word = words.splice(selection, 1)[0];
+		word = word[0].toUpperCase() + word.slice(1).toLowerCase();
+		randomEndpoint += word;
+	}
+	document.getElementById("404link").attributes.href.value = `/${randomEndpoint}`;
+};
